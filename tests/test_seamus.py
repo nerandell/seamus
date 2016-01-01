9e059409ae39ba814815c4db729f04fc0c70468a
nerandell <ankit.chandawala@kayako.com>
2016-01-01 16:11:07 +0530

    Using assertEqual instead of deprecated assertEquals

--- tests/test_seamus.py
+++ tests/test_seamus.py
@@ -11,24 +11,24 @@ class TestSeamus(TestCase):
 
     def test_use_function(self):
         self._seamus.use(self._original_func, *self._get_args(), **self._get_kwargs())
-        self.assertEquals(self._seamus._original_func, self._original_func)
-        self.assertEquals(self._seamus._original_func_args, (1, 2))
-        self.assertEquals({'kwarg1': 'test', 'kwarg2': 12}, self._seamus._original_func_kwargs)
+        self.assertEqual(self._seamus._original_func, self._original_func)
+        self.assertEqual(self._seamus._original_func_args, (1, 2))
+        self.assertEqual({'kwarg1': 'test', 'kwarg2': 12}, self._seamus._original_func_kwargs)
 
     def test_test_function(self):
         self._seamus.test(self._refactored_func, *self._get_args(), **self._get_kwargs())
-        self.assertEquals(self._seamus._refactored_func, self._refactored_func)
-        self.assertEquals(self._seamus._refactored_func_args, (1, 2))
-        self.assertEquals({'kwarg1': 'test', 'kwarg2': 12}, self._seamus._refactored_func_kwargs)
+        self.assertEqual(self._seamus._refactored_func, self._refactored_func)
+        self.assertEqual(self._seamus._refactored_func_args, (1, 2))
+        self.assertEqual({'kwarg1': 'test', 'kwarg2': 12}, self._seamus._refactored_func_kwargs)
 
     def test_run_function(self):
         self._seamus.use(self._original_func, *self._get_args(), **self._get_kwargs())
         self._seamus.test(self._refactored_func, *self._get_args(), **self._get_kwargs())
         result = self._seamus.run()
-        self.assertEquals(result, 1)
+        self.assertEqual(result, 1)
 
     def test_decorator(self):
-        self.assertEquals(self._decorated_function(*self._get_args(), **self._get_kwargs()), 1)
+        self.assertEqual(self._decorated_function(*self._get_args(), **self._get_kwargs()), 1)
 
     def test_decorator_with_insufficient_args(self):
         self.assertRaises(SeamusException, self._ill_decorated_function, *self._get_args(), **self._get_kwargs())
