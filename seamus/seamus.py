16121e4f340961ce0a5f136242e79cdeb29fab48
nerandell <ankit.chandawala@kayako.com>
2016-01-05 19:08:47 +0530

    Add documentation strings

--- seamus/seamus.py
+++ seamus/seamus.py
@@ -1,4 +1,7 @@
 class Seamus:
+    """
+    Main class responsible for handling the test.
+    """
     def __init__(self, comparator=None, run_strategy=None):
         self._original_func = None
         self._original_func_args = None
@@ -20,16 +23,35 @@ class Seamus:
         print(self._strategy)
 
     def use(self, original_func, *args, **kwargs):
+        """
+        Sets the function that is actually run. The return value
+        of this function is what will actually be returned when the
+        test is run
+        :param original_func: The function that is supposed to return the final result
+        :param args: Will be passed to the original_func as arguments
+        :param kwargs: Will be passed to the original_func as keyword arguments
+        """
         self._original_func = original_func
         self._original_func_args = args
         self._original_func_kwargs = kwargs
 
     def test(self, refactored_func, *args, **kwargs):
+        """
+        Sets the test function that will actually be run. The return value of this function
+        will be compared to the actual result and publish called with the comparison made.j
+        :param refactored_func: The function that is supposed to return the final result
+        :param args: Will be passed to the refactored_func as arguments
+        :param kwargs: Will be passed to the refactored_func as keyword arguments
+        """
         self._refactored_func = refactored_func
         self._refactored_func_args = args
         self._refactored_func_kwargs = kwargs
 
     def run(self):
+        """
+        Run the test
+        :return: Result of original_func
+        """
         actual_result = self._original_func(*self._original_func_args, **self._original_func_kwargs)
         if self._strategy():
             test_result = self._refactored_func(*self._refactored_func_args, **self._original_func_kwargs)
@@ -45,4 +67,8 @@ class Seamus:
         return actual_result == test_result
 
     def publish(self, is_equal):
+        """
+        :param is_equal: The value returned by self._comparator which compares values
+        returned by _original_func and _refactored_func
+        """
         pass
